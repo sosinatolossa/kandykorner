3e7a5d6a9c6328c95ab37d9bf2acd067d5a11096
sosinatolossa <sosinatolossa16@gmail.com>
2021-01-28 22:55:52 -0600

    have login/registration page now

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { Route, Redirect } from "react-router-dom";
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 import "./KandyKorner.css"
 
 export const KandyKorner = () => (
     <>
-        <NavBar />
-        <ApplicationViews />
+        <Route
+        render={() => {
+            if (localStorage.getItem("kandykorner_customer")) { //if the user is logged in, show them the stuff
+            return (
+                <>
+                <NavBar />
+                <ApplicationViews />
+                </>
+            );
+            } else {
+            return <Redirect to="/login" />; //if not redirect them to the login page
+            }
+        }}
+        />
+
+        <Route path="/login"> 
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
     </>
 )
